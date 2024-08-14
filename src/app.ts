5a6c9e0dfae9e8ca4e257eea2f7f73d18e640efd
Hamza <m.hamzakhaan@gmail.com>
2024-08-14 14:41:41 +0500

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
 const app = express();
 app.get("/", (req, res) => res.send("Welcome"));
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        error: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
+});
 export default app;
