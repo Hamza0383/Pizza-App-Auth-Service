973c815776a9fd258c4daad296ede435a349a3c8
Hamza <m.hamzakhaan@gmail.com>
2024-08-14 13:29:22 +0500

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import app from "./app";
 import { Config } from "./config";
-
+import logger from "./config/logger";
 function startServer() {
     const port = Config.PORT;
-
     try {
-        // eslint-disable-next-line no-console
-        app.listen(port, () => console.log(`server is runing at port ${port}`));
+        app.listen(port, () => logger.info(`server is runing at port ${port}`));
     } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log(error);
-        process.exit(1);
+        if (error instanceof Error) {
+            logger.error(error.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 }
 startServer();
