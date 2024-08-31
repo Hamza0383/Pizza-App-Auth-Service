ef098b9de98f84ef9117e04526ab78b88c3d2c37
Hamza <m.hamzakhaan@gmail.com>
2024-08-31 15:34:53 +0500

    test password hash

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,16 +3,18 @@ import { User } from "../entity/User";
 import { UserData } from "../types";
 import createHttpError from "http-errors";
 import { Roles } from "../constant";
-
+import bcrypt from "bcrypt";
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
     async create({ firstName, lastName, email, password }: UserData) {
+        const saltRounds = 10;
+        const hashPassword = await bcrypt.hash(password, saltRounds);
         try {
             return await this.userRepository.save({
                 firstName,
                 lastName,
                 email,
-                password,
+                password: hashPassword,
                 role: Roles.CUSTOMER,
             });
         } catch (err) {
