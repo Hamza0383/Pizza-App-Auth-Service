6b090b67a69c19d9f5c0612bbe5db4e445fb814d
Hamza <m.hamzakhaan@gmail.com>
2024-09-09 15:25:31 +0500

    refactor tokens

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,10 +6,12 @@ import { User } from "../entity/User";
 import logger from "../config/logger";
 import registerValidator from "../validators/register-validator";
 import { TokenService } from "../services/TokenService";
+import { RefreshToken } from "../entity/RefreshToken";
 const authRouter = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const tokenService = new TokenService();
+const refreshTokenRepository = AppDataSource.getRepository(RefreshToken);
+const tokenService = new TokenService(refreshTokenRepository);
 const authController = new AuthController(userService, logger, tokenService);
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 authRouter.post(
